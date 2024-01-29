5e9219d22026a5f3183fd1177e9bf00d7df33cf9
MohamdElbitar <83404626+MohamdElbitar@users.noreply.github.com>
2024-01-29 14:22:05 +0200

    up

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,13 @@ use App\Http\Controllers\SubscriptionTypesController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 
@@ -33,7 +33,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resources([
     'roles' => RoleController::class,
     'users' => UserController::class,
-    // 'products' => ProductController::class,
+
 ]);
 // routes/web.php
 
