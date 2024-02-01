ad2aaa21a08c0b27e674be81be1ec9cb1b95a380
Blue code <github@bluecode.sa>
2024-02-01 14:20:28 +0200

    up

--- resources/views/Admin/layouts/Inc/SuperAdmin.blade.php
+++ resources/views/Admin/layouts/Inc/SuperAdmin.blade.php
@@ -15,15 +15,7 @@
                 </li>
 
 
-                <li>
-                    <a href="javascript:void(0);" class="waves-effect"><i class="fas fa-lock"></i><span>
-                            @lang('Roles') &
-                            @lang('Permissions')<span class="float-right menu-arrow"><i
-                                    class="mdi mdi-chevron-right"></i></span> </span></a>
-                    <ul class="submenu">
 
-                    </ul>
-                </li>
 
                 <li>
                     <a href="javascript:void(0);" class="waves-effect"><i
