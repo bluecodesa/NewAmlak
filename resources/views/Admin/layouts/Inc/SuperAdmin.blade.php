fc4b0df04bbd0387a6e6346a22a3d2039fed1155
Blue code <github@bluecode.sa>
2024-02-01 14:19:08 +0200

    up

--- resources/views/Admin/layouts/Inc/SuperAdmin.blade.php
+++ resources/views/Admin/layouts/Inc/SuperAdmin.blade.php
@@ -25,6 +25,15 @@
                     </ul>
                 </li>
 
+                <li>
+                    <a href="javascript:void(0);" class="waves-effect"><i
+                            class="mdi mdi-bank-minus
+                        "></i><span> @lang('Subscriber management')<span
+                                class="float-right menu-arrow"><i class="mdi mdi-chevron-right"></i></span> </span></a>
+                    <ul class="submenu">
+                        <li><a href="{{ route('Admin.SubscriptionTypes.index') }}">@lang('Subscriptions')</a></li>
+                    </ul>
+                </li>
 
                 <li>
                     <a href="javascript:void(0);" class="waves-effect"><i
