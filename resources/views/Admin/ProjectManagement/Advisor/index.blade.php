62f0505ece9afbfc2bebaacb3d8a1ada98165c96
Amin Yasser <118663041+bluecodesa@users.noreply.github.com>
2024-02-14 16:38:17 +0200

    up

--- resources/views/Admin/ProjectManagement/Advisor/index.blade.php
+++ resources/views/Admin/ProjectManagement/Advisor/index.blade.php
@@ -45,7 +45,7 @@
                                                     <td>{{ $Advisor->email }}</td>
                                                     <td>{{ $Advisor->phone }}</td>
                                                     <td>{{ $Advisor->CityData->name }}</td>
-                                                    <td>{{ $Advisor->OfficeData->company_name }}</td>
+                                                    <td>{{ $Advisor->OfficeData->company_name ?? '' }}</td>
                                                     <td>
                                                         <a href="{{ route('Admin.Advisor.edit', $Advisor->id) }}"
                                                             class="btn btn-outline-info btn-sm waves-effect waves-light">@lang('Edit')</a>
