1f25d4fa93c3c0ce2a61ad9c0ea7651461bdbb6e
Amin Yasser <118663041+bluecodesa@users.noreply.github.com>
2024-02-18 13:07:46 +0200

    up

--- resources/views/Admin/layouts/navbar.blade.php
+++ resources/views/Admin/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
 
         <!-- LOGO -->
         <div class="topbar-left">
-            <a href="index.html" class="logo">
+            <a href="{{ route('home') }}" class="logo">
                 <span class="logo-light">
                     <img src="{{ url($sitting->icon) }}" width="80px" height="80px" alt="{{ $sitting->title }}"
                         style="border: 1px solid;
