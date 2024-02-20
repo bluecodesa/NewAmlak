b16077e36f261530001184ed4a6c53db3d576eda
Amin Yasser <github@bluecode.sa>
2024-02-20 14:38:14 +0200

    up

--- resources/views/Office/ProjectManagement/Project/CreateProperty.blade.php
+++ resources/views/Office/ProjectManagement/Project/CreateProperty.blade.php
@@ -1,7 +1,6 @@
 @extends('Admin.layouts.app')
 @section('title', __('Add new property'))
 @section('content')
-
     <div class="content-page">
         <!-- Start content -->
         <div class="content">
@@ -18,7 +17,6 @@
                         </div>
                     </div>
                 </div>
-
                 <div class="row">
                     <div class="col-12">
                         <div class="card m-b-30">
