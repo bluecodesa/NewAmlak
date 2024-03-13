0c0c5d5c0e91a5aeb4e4025a29a30a25f18fc46b
Amin Yasser <118663041+bluecodesa@users.noreply.github.com>
2024-03-13 15:52:14 +0200

    up

--- resources/views/Admin/settings/Notification/edit.blade.php
+++ resources/views/Admin/settings/Notification/edit.blade.php
@@ -16,7 +16,7 @@
                     <div class="row align-items-center">
                         <div class="col-sm-6">
                             <h4 class="page-title"> @lang('Edit') @lang('Notifications') :
-                                {{ __($notification->notification_name) }}</h4>
+                                {{ $notification->EmailTemplateData->subject ?? __($notification->notification_name) }}</h4>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-right">
