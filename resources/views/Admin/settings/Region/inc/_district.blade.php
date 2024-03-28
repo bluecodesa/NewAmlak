515edeffdc9c483d5d296901b18dbb46d7597de1
Amin Yasser <118663041+bluecodesa@users.noreply.github.com>
2024-03-28 03:04:24 +0200

    up

--- resources/views/Admin/settings/Region/inc/_district.blade.php
+++
@@ -1,5 +0,0 @@
-<option disabled selected value="">@lang('district')</option>
-@foreach ($districts as $district)
-    <option value="{{ $district->id }}">
-        {{ $district->name }}</option>
-@endforeach
--- resources/views/Broker/ProjectManagement/Project/Unit/inc/_owners.blade.php
+++
@@ -1,5 +0,0 @@
-<option disabled selected value="">@lang('owner name')</option>
-@foreach ($owners as $owner)
-    <option value="{{ $owner->id }}">
-        {{ $owner->name }}</option>
-@endforeach
