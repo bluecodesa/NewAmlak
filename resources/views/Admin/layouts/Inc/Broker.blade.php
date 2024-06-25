659b833de51a107b88d1b8181cd986b6bc99bc0b
Amin Yasser <118663041+bluecodesa@users.noreply.github.com>
2024-06-25 14:30:04 +0300

    up

--- resources/views/Admin/layouts/Inc/Broker.blade.php
+++ resources/views/Admin/layouts/Inc/Broker.blade.php
@@ -110,23 +110,7 @@
         {{-- </a> --}}
 
         <a href="{{ route('Broker.home') }}" class="app-brand-link">
-            <span class="app-brand-logo demo">
-                <svg width="32" height="22" viewBox="0 0 32 22" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
-                        fill="#7367F0" />
-                    <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
-                        d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z"
-                        fill="#161616" />
-                    <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
-                        d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z"
-                        fill="#161616" />
-                    <path fill-rule="evenodd" clip-rule="evenodd"
-                        d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
-                        fill="#7367F0" />
-                </svg>
-            </span>
+            <img src="{{ url($sitting->icon) }}" width="40" alt="">
             <span class="app-brand-text demo menu-text fw-bold ms-2 ps-1">{{ $sitting->title }}</span>
         </a>
 
@@ -147,7 +131,7 @@
             </a>
         </li>
         @if (in_array(15, $sectionsIds))
-        <li class="menu-item">
+            <li class="menu-item">
                 <a href="javascript:void(0);" class="menu-link menu-toggle">
                     {{-- <i class="menu-icon tf-icons ti ti-smart-home"></i> --}}
                     <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
@@ -160,63 +144,64 @@
                                 <div data-i18n="@lang('Projects')">@lang('Projects')</div>
                             </a>
                         </li>
-                        @endif
+                    @endif
 
-                        @if (Auth::user()->hasPermission('read-building'))
+                    @if (Auth::user()->hasPermission('read-building'))
                         <li class="menu-item">
-                                <a href="{{ route('Broker.Property.index') }}" class="menu-link">
-                                    <div data-i18n="@lang('properties')">@lang('properties')</div>
-                                </a>
-                            </li>
-                        @endif
-                        @if (Auth::user()->hasPermission('read-all-units'))
-                        <li class="menu-item">
-                                <a href="{{ route('Broker.Unit.index') }}" class="menu-link">
-                                    <div data-i18n="@lang('Units')">@lang('Units')</div>
-                                </a>
-                            </li>
-                        @endif
-                        <li class="menu-item">
-                                <a href="{{ route('Broker.Developer.index') }}" class="menu-link">
-                                    <div data-i18n="@lang('developers')">@lang('developers')</div>
-                                </a>
-                            </li>
-                        <li class="menu-item">
-                            <a href="{{ route('Broker.Advisor.index') }}" class="menu-link">
-                                <div data-i18n="@lang('advisors')">@lang('advisors')</div>
+                            <a href="{{ route('Broker.Property.index') }}" class="menu-link">
+                                <div data-i18n="@lang('properties')">@lang('properties')</div>
                             </a>
                         </li>
+                    @endif
+                    @if (Auth::user()->hasPermission('read-all-units'))
+                        <li class="menu-item">
+                            <a href="{{ route('Broker.Unit.index') }}" class="menu-link">
+                                <div data-i18n="@lang('Units')">@lang('Units')</div>
+                            </a>
+                        </li>
+                    @endif
+                    <li class="menu-item">
+                        <a href="{{ route('Broker.Developer.index') }}" class="menu-link">
+                            <div data-i18n="@lang('developers')">@lang('developers')</div>
+                        </a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="{{ route('Broker.Advisor.index') }}" class="menu-link">
+                            <div data-i18n="@lang('advisors')">@lang('advisors')</div>
+                        </a>
+                    </li>
                 </ul>
             </li>
         @endif
 
         @if (in_array(18, $sectionsIds))
-        <li class="menu-item">
+            <li class="menu-item">
                 <a href="javascript:void(0);" class="menu-link menu-toggle">
                     {{-- <i class="menu-icon tf-icons ti ti-smart-home"></i> --}}
                     <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
                     <div data-i18n="@lang('Gallary Mange')">@lang('Gallary Mange')</div>
                 </a>
                 <ul class="menu-sub">
+                    <li class="menu-item">
+                        <a href="{{ route('Broker.Gallery.index') }}" class="menu-link">
+                            <div data-i18n="@lang('Properties Gallary')">@lang('Properties Gallary')</div>
+                        </a>
+                    </li>
+
+                    @if (Auth::user()->hasPermission('read-requests-interest'))
                         <li class="menu-item">
-                            <a href="{{ route('Broker.Gallery.index') }}" class="menu-link">
-                                <div data-i18n="@lang('Properties Gallary')">@lang('Properties Gallary')</div>
+                            <a href="{{ route('Broker.Gallary.showInterests') }}" class="menu-link">
+                                <div data-i18n="@lang('Requests for interest')">@lang('Requests for interest')</div>
                             </a>
                         </li>
-
-                        @if (Auth::user()->hasPermission('read-requests-interest'))                        <li class="menu-item">
-                                <a href="{{ route('Broker.Gallary.showInterests') }}" class="menu-link">
-                                    <div data-i18n="@lang('Requests for interest')">@lang('Requests for interest')</div>
-                                </a>
-                            </li>
-                        @endif
+                    @endif
 
                 </ul>
             </li>
         @endif
 
         @if (in_array(16, $sectionsIds))
-        <li class="menu-item">
+            <li class="menu-item">
                 <a href="{{ route('Broker.Owner.index') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
                     <div data-i18n="@lang('Owners Management')">@lang('Owners Management')</div>
@@ -225,7 +210,7 @@
         @endif
 
         @if (in_array(12, $sectionsIds))
-        <li class="menu-item">
+            <li class="menu-item">
                 <a href="{{ route('Broker.ShowSubscription') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
                     <div data-i18n="@lang('Subscription Management')">@lang('Subscription Management')</div>
@@ -233,7 +218,7 @@
             </li>
         @endif
         @if (in_array(11, $sectionsIds))
-        <li class="menu-item">
+            <li class="menu-item">
                 <a href="{{ route('Broker.Tickets.index') }}" class="menu-link">
                     <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
                     <div data-i18n="@lang('technical support')">@lang('technical support')</div>
@@ -241,12 +226,12 @@
             </li>
         @endif
         @if (in_array(9, $sectionsIds))
-        <li class="menu-item">
-            <a href="{{ route('Broker.Setting.index') }}" class="menu-link">
-                <i class="menu-icon tf-icons ti ti-app-window"></i>
-                <div data-i18n="@lang('Settings')">@lang('Settings')</div>
-            </a>
-        </li>
+            <li class="menu-item">
+                <a href="{{ route('Broker.Setting.index') }}" class="menu-link">
+                    <i class="menu-icon tf-icons ti ti-app-window"></i>
+                    <div data-i18n="@lang('Settings')">@lang('Settings')</div>
+                </a>
+            </li>
         @endif
 
         {{-- <li class="menu-item">
