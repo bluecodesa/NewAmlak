af8f268ea1864bd8336c27541c968d24e9f95dd0
Amin Yasser <118663041+bluecodesa@users.noreply.github.com>
2024-06-30 17:15:00 +0300

    up

--- resources/views/Admin/subscribers/index.blade.php
+++ resources/views/Admin/subscribers/index.blade.php
@@ -208,7 +208,7 @@
                                     </td>
                                 </tr>
                             @empty
-                                <td colspan="6">
+                                <td colspan="7">
                                     <div class="alert alert-danger d-flex align-items-center" role="alert">
                                         <span class="alert-icon text-danger me-2">
                                             <i class="ti ti-ban ti-xs"></i>
