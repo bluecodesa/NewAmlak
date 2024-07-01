a628b1aa998d360153c4575830c2fde7d8facc9f
Amin Yasser <118663041+bluecodesa@users.noreply.github.com>
2024-07-01 15:27:48 +0300

    up

--- app/Services/UserCreationService.php
+++ app/Services/UserCreationService.php
@@ -8,11 +8,25 @@ class UserCreationService
 {
     public function createBroker(array $userData)
     {
+        $Last_customer_id = User::where('customer_id', '!=', null)->latest()->value('customer_id');
+        $delimiter = '-';
+        $prefixes = ['AMK1-', 'AMK2-', 'AMK3-', 'AMK4-', 'AMK5-', 'AMK6-'];
+        if (!$Last_customer_id) {
+            $new_customer_id = 'AMK1-0001';
+        } else {
+            $result = explode($delimiter, $Last_customer_id);
+            $number = (int)$result[1] + 1;
+            $tag_index = min(intval($number / 1000), count($prefixes) - 1);
+            $tag = $prefixes[$tag_index];
+            $new_customer_id = $tag . str_pad($number % 1000, 4, '0', STR_PAD_LEFT);
+        }
+
         return User::create([
             'is_broker' => 1,
             'name' => $userData['name'],
             'email' => $userData['email'],
             'user_name' => uniqid(),
+            'customer_id' => $new_customer_id,
             'password' => bcrypt($userData['password']),
             'avatar' => $userData['broker_logo'] ?? null,
 
@@ -21,6 +35,20 @@ class UserCreationService
 
     public function createOffice(array $userData)
     {
+        $Last_customer_id = User::where('customer_id', '!=', null)->latest()->value('customer_id');
+        $delimiter = '-';
+        $prefixes = ['AMK1-', 'AMK2-', 'AMK3-', 'AMK4-', 'AMK5-', 'AMK6-'];
+        if (!$Last_customer_id) {
+            $new_customer_id = 'AMK1-0001';
+        } else {
+            $result = explode($delimiter, $Last_customer_id);
+            $number = (int)$result[1] + 1;
+            $tag_index = min(intval($number / 1000), count($prefixes) - 1);
+            $tag = $prefixes[$tag_index];
+            $new_customer_id = $tag . str_pad($number % 1000, 4, '0', STR_PAD_LEFT);
+        }
+
+
         return User::create([
             'is_office' => 1,
             'name' => $userData['name'],
@@ -28,6 +56,7 @@ class UserCreationService
             'key_phone' => $userData['key_phone'],
             'full_phone' => $userData['full_phone'],
             'user_name' => uniqid(),
+            'customer_id' => $new_customer_id,
             'password' => bcrypt($userData['password']),
             'avatar' => $userData['company_logo'] ?? null,
         ]);
