fe8d100b3d0eaeebb19a366899d4a859f4a7255a
MohamdElbitar <83404626+MohamdElbitar@users.noreply.github.com>
2024-07-10 17:43:45 +0300

    up

--- resources/views/Office/ProjectManagement/Renter/inc/search-result-modal.blade.php
+++ resources/views/Office/ProjectManagement/Renter/inc/search-result-modal.blade.php
@@ -1,5 +1,5 @@
 <!-- resources/views/Office/ProjectManagement/Renter/inc/_result.blade.php -->
-<div class="alert alert-success">{{ $message }}</div>
+<div class="alert alert-success"><span class="required-color">{{ $message }}</span></div>
 <p>Name: {{ $user->name }}</p>
 <p>Email: {{ $user->email }}</p>
 <p>Phone: {{ $user->full_phone }}</p>
