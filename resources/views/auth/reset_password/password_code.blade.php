2994f92abff46436dbfc73bc987f225989e42d9f
MohamdElbitar <83404626+MohamdElbitar@users.noreply.github.com>
2024-07-16 14:41:01 +0300

    up

--- resources/views/auth/reset_password/password_code.blade.php
+++ resources/views/auth/reset_password/password_code.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    {!! $sitting->google_tag !!}
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reset Password Code</title>
@@ -8,5 +10,7 @@
 <body>
     <p>Your reset password code is: <strong>{{ $code }}</strong></p>
     <p>Please use this code to reset your password. This code will expire in 30 minutes.</p>
+    {!! $sitting->zoho_salesiq !!}
+
 </body>
 </html>
