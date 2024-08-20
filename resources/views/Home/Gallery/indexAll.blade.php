23f3450e1ae49d67ee96bea6262e30c6cf88cdc3
MohamdElbitar <83404626+MohamdElbitar@users.noreply.github.com>
2024-08-20 18:21:04 +0300

    up

--- resources/views/Home/Gallery/indexAll.blade.php
+++ resources/views/Home/Gallery/indexAll.blade.php
@@ -32,8 +32,20 @@
                                                 @foreach($advertisings as $index => $advertisement)
                                                     <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
                                                         @if(in_array(pathinfo($advertisement->content, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png', 'gif']))
-                                                            <a href="{{ $advertisement->ad_url ?? 'javascript:0;' }}" target="_blank">
+                                                        @if($advertisement->ad_url)
+                                                            <a href="{{ $advertisement->ad_url }}" target="_blank">
                                                                 <img src="{{ asset($advertisement->content) }}" class="d-block w-100 h-100" alt="Advertisement Image">
+                                                            </a>
+                                                        @else
+                                                            <img src="{{ asset($advertisement->content) }}" class="d-block w-100 h-100" alt="Advertisement Image">
+                                                        @endif
+                                                    @else
+                                                        @if($advertisement->ad_url)
+                                                            <a href="{{ $advertisement->ad_url }}" target="_blank">
+                                                                <video class="d-block w-100 h-100" autoplay muted>
+                                                                    <source src="{{ asset($advertisement->content) }}" type="video/mp4">
+                                                                    @lang('Your browser does not support the video tag.')
+                                                                </video>
                                                             </a>
                                                         @else
                                                             <video class="d-block w-100 h-100" autoplay muted>
@@ -41,6 +53,8 @@
                                                                 @lang('Your browser does not support the video tag.')
                                                             </video>
                                                         @endif
+                                                    @endif
+                                                    
                                                     </div>
                                                 @endforeach
                                             </div>
