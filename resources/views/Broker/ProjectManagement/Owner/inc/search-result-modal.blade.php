af157aa78fb9f4f0f94e274673c2d18eea237627
MohamdElbitar <83404626+MohamdElbitar@users.noreply.github.com>
2024-09-05 17:48:06 +0300

    up

--- resources/views/Broker/ProjectManagement/Owner/inc/search-result-modal.blade.php
+++ resources/views/Broker/ProjectManagement/Owner/inc/search-result-modal.blade.php
@@ -1,7 +1,7 @@
 <div class="alert alert-success"><span class="required-color">{{ $message }}</span></div>
 <p>@lang('Name'): {{ $user->name }}</p>
 <p>@lang('ID Number'): {{ $user->id_number }}</p>
-@if($user->UserBrokerData)
+{{-- @if($user->UserBrokerData)
 <p>@lang('City'): {{ $user->UserBrokerData->CityData->name }}</p>
 @elseif($user->UserOfficeData)
 <p>@lang('City'): {{ $user->UserOfficeData->CityData->name }}</p>
@@ -11,7 +11,7 @@
 <p>@lang('City'): {{ $user->UserRenterData->CityData->name }}</p>
 @elseif ($user->UserEmployeeData)
 <p>@lang('City'): {{ $user->UserEmployeeData->CityData->name }}</p>
-@endif
+@endif --}}
 
 <form id="addAsOwnerForm" action="{{ route('Broker.Owner.addAsOwner', $user->id) }}" method="POST">
     @csrf
