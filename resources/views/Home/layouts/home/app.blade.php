81b8aaac79c7ef90c75f566553e752090f3619a9
MohamdElbitar <83404626+MohamdElbitar@users.noreply.github.com>
2024-10-07 17:48:49 +0300

    up

--- resources/views/Home/layouts/home/app.blade.php
+++ resources/views/Home/layouts/home/app.blade.php
@@ -25,7 +25,7 @@
         <div class="container">
             <div class="navbar navbar-expand-lg landing-navbar px-3 px-md-4 py-0">
                 <!-- Menu logo wrapper: Start -->
-                <div class="navbar-brand app-brand demo d-flex py-0 py-lg-2 me-4">
+                <div class="navbar-brand app-brand demo d-flex py-0 me-4">
                     <!-- Mobile menu toggle: Start-->
                     <button class="navbar-toggler border-0 px-0 me-2" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
