0762e2c9e32ffd1c5ee57dbf58b4aa224a77206a
MohamdElbitar <mohamedezzat14995@gmail.com>
2024-10-15 11:53:46 +0300

    up

--- app/Http/Traits/WhatsApp/WhatsappSendCode.php
+++ app/Http/Traits/WhatsApp/WhatsappSendCode.php
@@ -3,7 +3,9 @@
 namespace App\Http\Traits\WhatsApp;
 
 use App\Models\WhatsAppSetting;
+use App\Models\WhatsappTemplate;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\DB;
 
 trait WhatsappSendCode
 {
@@ -11,36 +13,42 @@ trait WhatsappSendCode
     {
         $whatsAppSetting = WhatsAppSetting::first();
 
-        if (!$whatsAppSetting) {
-            return 'No WhatsApp settings found for this user';
-        }
+        $notification_id = DB::table('notification_settings')->where('notification_name', 'add-new-property-finder')->where('whatsapp',1)->value('id');
+        $whatsappTemplate = WhatsappTemplate::where('notification_setting_id', $notification_id)->first();
+    if (!$whatsAppSetting) {
+        return 'No WhatsApp settings found for this user';
+    }
 
-        $client = new Client();
+        if ($whatsappTemplate) {
 
-        try {
-            $response = $client->post(
-                $whatsAppSetting->url, 
-                [
-                    'headers' => [
-                        'Authorization' => 'Bearer ' . $whatsAppSetting->api_key,
-                        'Content-Type' => 'application/json',
-                        'Accept' => 'application/json',
-                    ],
 
-                    'json' => [
-                        'session_uuid' => $whatsAppSetting->session_uuid, 
-                        'phone' => $data['phone'],
-                        'type' => $whatsAppSetting->type,
-                        'message' => __('Welcome Your OTP code is') . ' : ' . $data['otp'],
-                        'schedule_at' => now(),
-                    ],
-                ]
-            );
+            $client = new Client();
 
-            return $response->getBody()->getContents();
-        } catch (\Throwable $th) {
-            // Handle any errors that occur during the request
-            return 'Error sending WhatsApp message: ' . $th->getMessage();
+            try {
+                $response = $client->post(
+                    $whatsAppSetting->url,
+                    [
+                        'headers' => [
+                            'Authorization' => 'Bearer ' . $whatsAppSetting->api_key,
+                            'Content-Type' => 'application/json',
+                            'Accept' => 'application/json',
+                        ],
+
+                        'json' => [
+                            'session_uuid' => $whatsAppSetting->session_uuid,
+                            'phone' => $data['phone'],
+                            'type' => $whatsAppSetting->type,
+                            'message' => __($whatsappTemplate->content) . __('This is the OTP code:') . $data['otp'],
+                            'schedule_at' => now(),
+                        ],
+                    ]
+                );
+
+                return $response->getBody()->getContents();
+            } catch (\Throwable $th) {
+                // Handle any errors that occur during the request
+                return 'Error sending WhatsApp message: ' . $th->getMessage();
+            }
         }
     }
 }
