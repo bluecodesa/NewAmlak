f9e425423e11eaf701ecf363ff544ec70e774829
MohamdElbitar <mohamedezzat14995@gmail.com>
2024-10-20 22:54:53 +0300

    up

--- resources/views/Home/Gallery/_inc_main_gallery/interactive_map.blade.php
+++ resources/views/Home/Gallery/_inc_main_gallery/interactive_map.blade.php
@@ -1,3 +1,4 @@
+<!-- Home/Gallery/_inc_main_gallery/interactive_map.blade.php -->
 <div id="map" style="height: 100vh;"></div>
 <link href='https://api.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css' rel='stylesheet' />
 
@@ -10,7 +11,6 @@
 
         let mapInitialized = false; // To avoid re-initializing the map
         var items = @json($allItems);
-        var falLicenseUser = $falLicenseUser;
 
 document.querySelector('button[data-bs-target="#navs-justified-gallery"]').addEventListener('click', function() {
     if (!mapInitialized) {
@@ -24,8 +24,7 @@ document.querySelector('button[data-bs-target="#navs-justified-gallery"]').addEv
 
         // Optional: Add map controls
         map.addControl(new mapboxgl.NavigationControl());
-
-        // Function to add markers
+        // Optional: Add markers based on your items
         function addMarkers(filteredItems) {
             filteredItems.forEach(function(item) {
                 if (item.lat_long) {
@@ -34,83 +33,72 @@ document.querySelector('button[data-bs-target="#navs-justified-gallery"]').addEv
                     var rentPriceAndType = '';
 
                     var galleryName = item.broker_data ? item.broker_data.gallery_data?.gallery_name :
-                                      item.office_data ? item.office_data.gallery_data?.gallery_name : null;
+                                      item.office_data ? item.office_data.gallery_data?.gallery_name : '';
 
                     // Determine the correct show route and rent price/type
                     if (item.isGalleryUnit) {
                         showRoute = `{{ route('gallery.showUnitPublic', ['gallery_name' => ':gallery_name', 'id' => ':id']) }}`
                                     .replace(':gallery_name', galleryName)
                                     .replace(':id', item.id);
-                                    console.log(galleryName);
 
                         rentPriceAndType = `${item.rentPrice} @lang('SAR') / ${item.rent_type_show}`;
                     } else if (item.isGalleryProject) {
                         showRoute = `{{ route('Home.showPublicProject', ['gallery_name' => ':gallery_name', 'id' => ':id']) }}`
                                     .replace(':gallery_name', galleryName)
                                     .replace(':id', item.id);
-                                    console.log(galleryName);
 
                     } else if (item.isGalleryProperty) {
                         showRoute = `{{ route('Home.showPublicProperty', ['gallery_name' => ':gallery_name', 'id' => ':id']) }}`
                                     .replace(':gallery_name', galleryName)
                                     .replace(':id', item.id);
-                                    console.log(galleryName);
 
                     }
 
                     // Create the map marker and popup content
-                    @if (
-                        falLicenseUser &&
-                        falLicenseUser->ad_license_status == 'valid' &&
-                        falLicenseUser->falData->for_gallery == 1 &&
-                        item->ad_license_status == 'Valid'
-                    )
-                        new mapboxgl.Marker()
-                            .setLngLat([parseFloat(coordinates[1]), parseFloat(coordinates[0])])
-                            .setPopup(new mapboxgl.Popup({ offset: 25 })
-                                .setHTML(`
-                                    <div id="popup-${item.id}" style="width: 200px; cursor: pointer; display: flex; flex-direction: column; align-items: center; text-align: center;">
-                                        <h6>${item.name || item.ad_name}</h6>
-                                        ${!item.isGalleryProject ? `
-                                            <p>
-                                                <i class="ti ti-building-arch"></i> ${item.property_type_data ? item.property_type_data.name : ''} / ${item.type ? item.type : ''}
-                                            </p>
-                                        ` : ''}
-                                        ${item.isGalleryUnit ? `
-                                            <p>
-                                                <i class="ti ti-bell-dollar"></i> ${rentPriceAndType ? `<span class="pb-1">${rentPriceAndType}</span>` : ''}
-                                            </p>
-                                        ` : ''}
+                    new mapboxgl.Marker()
+                        .setLngLat([parseFloat(coordinates[1]), parseFloat(coordinates[0])])
+                        .setPopup(new mapboxgl.Popup({ offset: 25 })
+                            .setHTML(`
+                                <div id="popup-${item.id}" style="width: 200px; cursor: pointer; display: flex; flex-direction: column; align-items: center; text-align: center;">
+                                    <h6>${item.name || item.ad_name}</h6>
+                                    ${!item.isGalleryProject ? `
                                         <p>
-                                            ${item.isGalleryUnit ?
-                                                (item.ProjectData ? `<span class="badge bg-label-secondary mt-1">${item.ProjectData.name}</span>` : '') +
-                                                " " +
-                                                (item.PropertyData ? `<span class="badge bg-label-secondary mt-1">${item.PropertyData.name}</span>` : '') +
-                                                ` <span class="badge bg-label-secondary mt-1">@lang('Unit')</span>`
-                                            : item.isGalleryProperty ?
-                                                (item.ProjectData ? `<span class="badge bg-label-secondary mt-1">${item.ProjectData.name}</span>` : '') +
-                                                ` <span class="badge bg-label-secondary mt-1">@lang('property')</span>`
-                                            : item.isGalleryProject ?
-                                                `<span class="badge bg-label-secondary mt-1">@lang('Project')</span>`
-                                            : ''}
+                                            <i class="ti ti-building-arch"></i> ${item.property_type_data ? item.property_type_data.name : ''} / ${item.type ? item.type : ''}
                                         </p>
+                                    ` : ''}
+                                    ${item.isGalleryUnit ? `
                                         <p>
-                                            <i class="ti ti-map-pin"></i> ${item.city_data ? item.city_data.name : ''}
+                                            <i class="ti ti-bell-dollar"></i> ${rentPriceAndType ? `<span class="pb-1">${rentPriceAndType}</span>` : ''}
                                         </p>
-                                        <a href="${showRoute}" target="_blank" class="btn btn-primary mt-2">@lang('Show')</a>
-                                    </div>
-                                `))
-                            .addTo(map);
-                    @endif
+                                    ` : ''}
+                                    <p>
+                                        ${item.isGalleryUnit ?
+                                            (item.ProjectData ? `<span class="badge bg-label-secondary mt-1">${item.ProjectData.name}</span>` : '') +
+                                            " " +
+                                            (item.PropertyData ? `<span class="badge bg-label-secondary mt-1">${item.PropertyData.name}</span>` : '') +
+                                            ` <span class="badge bg-label-secondary mt-1">@lang('Unit')</span>`
+                                        : item.isGalleryProperty ?
+                                            (item.ProjectData ? `<span class="badge bg-label-secondary mt-1">${item.ProjectData.name}</span>` : '') +
+                                            ` <span class="badge bg-label-secondary mt-1">@lang('property')</span>`
+                                        : item.isGalleryProject ?
+                                            `<span class="badge bg-label-secondary mt-1">@lang('Project')</span>`
+                                        : ''}
+                                    </p>
+                                    <p>
+                                        <i class="ti ti-map-pin"></i> ${item.city_data ? item.city_data.name : ''}
+                                    </p>
+                                    <a href="${showRoute}" target="_blank" class="btn btn-primary mt-2">@lang('Show')</a>
+                                </div>
+                            `))
+                        .addTo(map);
                 }
             });
         }
+    // Initial markers for My Properties
+    addMarkers(items);
 
-        // Initial markers for My Properties
-        addMarkers(items);
-
-        mapInitialized = true;
-    }
-});
-});
+                mapInitialized = true;
+            }
+        });
+    });
 </script>
