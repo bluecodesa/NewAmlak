344cab4cd1c918271f24e73b854ee08482bbe9da
MohamdElbitar <mohamedezzat14995@gmail.com>
2024-10-28 11:41:08 +0300

    up

--- resources/views/Admin/layouts/Inc/Office.blade.php
+++ resources/views/Admin/layouts/Inc/Office.blade.php
@@ -1,7 +1,12 @@
-@php
+{{-- @php
     $sectionsIds = Auth::user()
         ->UserOfficeData?->UserSubscription?->SubscriptionTypeData?->sections()
         ->pluck('section_id')
+        ->toArray();
+@endphp --}}
+@php
+    $sectionsIds = Auth::user()
+        ->UserOfficeData->UserSubscription->SubscriptionSectionData->pluck('section_id')
         ->toArray();
 @endphp
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
