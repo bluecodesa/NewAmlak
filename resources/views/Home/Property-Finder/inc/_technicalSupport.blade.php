337924a90edf0218631ac8c776f32a1727c891b6
MohamdElbitar <mohamedezzat14995@gmail.com>
2024-11-06 17:14:37 +0200

    up

--- resources/views/Home/Property-Finder/inc/_technicalSupport.blade.php
+++ resources/views/Home/Property-Finder/inc/_technicalSupport.blade.php
@@ -164,8 +164,8 @@
                     </div>
 
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Close') }}</button>
-                        <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('close') }}</button>
+                        <button type="submit" class="btn btn-primary">{{ __('save') }}</button>
                     </div>
                 </form>
             </div>
