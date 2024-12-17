95858faadfad11a071628ab5d59a07b6c4b82735
MohamdElbitar <mohamedezzat14995@gmail.com>
2024-12-17 10:18:04 +0200

    up

--- resources/views/Home/layouts/home/app.blade.php
+++ resources/views/Home/layouts/home/app.blade.php
@@ -226,7 +226,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();document.getElementById('logout-form').submit();"
                                             class="dropdown-item" target="_blank"><i
-                                                class="tf-icons ti ti-logout scaleX-n1-rtl me-md-1"></i>تسجيل خروج</a>
+                                                class="tf-icons ti ti-logout scaleX-n1-rtl me-md-1"></i> @lang('Log Out')</a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST"
                                             style="display: none;">
                                             @csrf
